2174e74b46556de5f31cd2fe3c37cfa56b89b440
Rasel Sheikh <64516910+Rasel-bot@users.noreply.github.com>
2022-06-21 11:16:55 +0600

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,18 @@
+def binary_search(L,x):
+	left, right = 0, len(L)-1
+	
+	while left <= right :
+		mid = left + right//2
+		if L[mid] == x:
+			return mid
+		elif L[mid]<x:
+			left = mid+1
+		else:
+			right = mid-1
+	return -1
+	
+L =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+x = 11
+output = binary_search(L,x)
+print(output)
+			
